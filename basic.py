f87ef97e8fbb44873ba2e6f2a036bee963207bd8
gourav017 <gourav6471sahu@gmail.com>
2026-05-01 18:17:27 +0530

    python basic syn

---
+++ basic.py
@@ -0,0 +1,14 @@
+cource_name = "python programming"
+watching_person = 1000
+is_published = False
+print(cource_name)
+
+first_name = "gourav"
+last_name = "sahu"
+full = first_name + " " + last_name
+
+print(full)
+
+full_name_formation_f = f"{first_name} {last_name}"
+full_name_formation_f = f"{len(first_name)} {last_name}"
+print(full_name_formation_f)
---
+++ string.py
@@ -0,0 +1,6 @@
+# string slice
+couurce_name = "python progmamming"
+print(len(couurce_name))
+
+print(couurce_name[0])
+print(couurce_name[0:4])
