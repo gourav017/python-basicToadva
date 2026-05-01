9c2be622c6cfa6345dbfa991bb578d3cd5c995fb
gourav017 <gourav6471sahu@gmail.com>
2026-05-01 18:43:22 +0530

    numbers and import modules-math

---
+++ number.py
@@ -0,0 +1,18 @@
+import math
+print(5 + 5)
+print(5 * 1)
+print(5-4)
+print(5/2)
+print(5//2)
+print(5 % 3)
+print(5**2)
+
+x = 10
+x += 5
+print(x)
+
+print(round(2.555))
+print(abs(-2))
+# in python we have various module that can be import to do various things ex math module
+
+print(math.ceil(2.3))
