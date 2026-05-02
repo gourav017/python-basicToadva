ce4ea6735eee12d6f074ad98b86dad24c811119a
gourav017 <gourav6471sahu@gmail.com>
2026-05-02 16:29:36 +0530

    if type c

---
+++ ifelse.py
@@ -0,0 +1,14 @@
+age = 2
+if age < 18:
+    print("you are a minor")
+
+
+print("done")
+
+if age < 18:
+    print("you are a minor")
+
+elif age >= 18 and age < 65:
+    print("you are an adult")
+
+print('end')
---
+++ type_converstion.py
@@ -0,0 +1,4 @@
+x = input("x:")
+print(type(x))
+y = int(x) + 2
+print(y)
